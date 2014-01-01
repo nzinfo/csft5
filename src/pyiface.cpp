ebb517e7f05433544e55115b93f52c7b4bf61133
nzinfo <li.monan@gmail.com>
2014-01-01 18:45:29 +0800

    import pyiface

---
+++ src/pyiface.cpp
@@ -0,0 +1,16 @@
+#include "pyiface.h"
+
+//------ Python Configure Block -------
+
+
+//------ Python Data Source Block -------
+
+
+//------ Python Tokenizer Block -------
+
+
+//------ Python Cache Block -------
+
+
+//------ Python Query Block -------
+
